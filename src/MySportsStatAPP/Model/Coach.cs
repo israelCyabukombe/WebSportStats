29f7b037cdc5b9dd3ddeae3a448288d6b679a94f
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-16 15:24:52 -0500

    Add a Coach Entity to the Data Base Model

---
+++ src/MySportsStatAPP/Model/Coach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySportsStatAPP.Model
+{
+    public class Coach
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int TeamID { get; set; }
+    }
+}
