8ebf6b8c61f0083db5751ca10a0eab3673a8495a
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-16 15:28:03 -0500

    corrected syntax error

--- src/MySportsStatAPP/Controllers/HomeController.cs
+++ src/MySportsStatAPP/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MySportsStatAPP.Controllers
