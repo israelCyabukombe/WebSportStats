3014c474e2b37c9da0327cf35bca323e686c304f
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-16 15:26:15 -0500

    Added a Team Entity class to the Data base

---
+++ src/MySportsStatAPP/Model/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySportsStatAPP.Model
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public int CoachID { get; set; }
+    }
+}
