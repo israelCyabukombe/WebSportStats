f2b22bcbd0b44b27af050eca6fc35740f12664d5
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-16 15:27:14 -0500

    Added a SportsDbContext that inherites from the Entity Framework DBContext

---
+++ src/MySportsStatAPP/Model/SportsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MySportsStatAPP.Model
+{
+    public class SportsDbContext: DbContext
+    {
+        public SportsDbContext(DbContextOptions<SportsDbContext> options) : base(options)
+        {
+         }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>().ToTable("Player");
+            modelBuilder.Entity<Coach>().ToTable("Coach");
+            modelBuilder.Entity<Team>().ToTable("Team");
+
+        }
+    }
+}
