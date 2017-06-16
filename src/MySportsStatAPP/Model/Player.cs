8db3126a8243a7e12f369444e7f1b3deb070b886
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-16 15:25:39 -0500

    Added a PlayerID entity to the Data base

---
+++ src/MySportsStatAPP/Model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySportsStatAPP.Model
+{
+    public class Player
+    {
+         public int PlayerID { get; set; }
+         public string FirstName { get; set; }
+         public string LastName { get; set; }
+
+
+    }
+}
