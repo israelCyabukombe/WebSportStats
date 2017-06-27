cf59965a7e1532d9a0598fa2a69a30d835a128a8
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-26 20:52:03 -0500

    Added Validation to the Team.cs Model
    i.e. [Required]  and [MaxLength]

--- src/MySportsStatAPP/Model/Team.cs
+++ src/MySportsStatAPP/Model/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace MySportsStatAPP.Model
 {
     public class Team
     {
+        [Required]
         public int TeamID { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string TeamName { get; set; }
+        [Required]
         public int CoachID { get; set; }
+        public List<Player> players{ get; set; }
     }
 }
