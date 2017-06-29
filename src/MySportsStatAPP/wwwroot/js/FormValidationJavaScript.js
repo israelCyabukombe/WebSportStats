a89129265a070ca434387eeaad4fcc9042c58540
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-28 19:27:13 -0500

    Added this javascript file form validation Since I'm having trouble scoping 3 asp models in my view files

---
+++ src/MySportsStatAPP/wwwroot/js/FormValidationJavaScript.js
@@ -0,0 +1,11 @@
+﻿function MyFormValidation()
+{
+    for (var i = 0; i < MyForm.elements.length; i++) {
+        if(MyForm.elements[i].className=="required" && MyForm.elements[i].value.length==0)
+        {
+            alert('Please fill in all the required fields "*"');
+            return false;
+        }
+    }
+
+}
