4484468dfb2509992113affc20dd1a1f763e7ef6
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-28 19:39:44 -0500

    Added a viewModel This helps have access to our models alltogether for instance if we wanted to display the contents of our database in one view

---
+++ src/MySportsStatAPP/ViewModels/CoachTeamPlayerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MySportsStatAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySportsStatAPP.ViewModels
+{
+    public class CoachTeamPlayerViewModel
+    {
+        public Team myTeam { get; set; }
+        public Coach myCoach { get; set; }
+
+        public Player myPlayer { get; set; }
+    }
+}
