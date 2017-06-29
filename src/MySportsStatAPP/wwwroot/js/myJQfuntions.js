9d50b97fb7f9dec83a5873b93d4ced149ff7dcef
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-06-28 19:26:21 -0500

    Added some JQuery  functions to hide forms

---
+++ src/MySportsStatAPP/wwwroot/js/myJQfuntions.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#TeamCoach").click(function () {
+        $('#TeamCoachForm').show();
+    });
+
+    $("#Players").click(function () {      
+        $('#PlayerForm').show();
+    });
+
+
+});
